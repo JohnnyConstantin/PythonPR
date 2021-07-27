534877dff9089f6280a4d6bc21edd4fca6c1ef86
Johnny <zkonst12@gmail.com>
2021-07-27 18:37:44 +0300

    Prac with sql

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import sqlite3
+
 def main():
-    print("test")
+    conn = sqlite3.connect('identifier.sqlite')
+    cur = conn.cursor()
+    cur.execute("SELECT ((2*MAX(two) - SUM(two)) + (2*MAX(four) - SUM(four)) + (2*MAX(three) - SUM(three))) FROM test GROUP BY five")
+    one_result = cur.fetchmany(5)
+    print(one_result)
+
 
 if __name__ == '__main__':
     main()
